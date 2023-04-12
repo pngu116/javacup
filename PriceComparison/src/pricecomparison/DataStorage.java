b1a3a3be255a68f18d627f914769ec4aa211d7cd
timmyttran02 <72949286+timmyttran02@users.noreply.github.com>
2023-04-11 19:28:59 -0500

    Update DataStorage.java

--- PriceComparison/src/pricecomparison/DataStorage.java
+++ PriceComparison/src/pricecomparison/DataStorage.java
@@ -1,48 +1,58 @@
 package pricecomparison;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.ArrayList;
+/**
+ * DataStorage --- program that gets product information from productData.txt
+ * file
+ *
+ * @author Tam Nguyen
+ */
+public class DataStorage extends Database {
 
-//gets product information from txt file
-public class DataStorage extends Database{
     ProductData pd = new ProductData();
-    public String[] price;
-    public String[] name;
-    public String[] store;
+    private String[] price;
+    private String[] name;
+    private String[] store;
     private String[][] prod = new String[21][3];
-    
-    // getProductPrice() gets product price from productData.txt
+
+    /**
+     * Gets product price from productData.txt
+     *
+     * @return price
+     */
     public String[] getProductPrice() {
-        String[]price = pd.getData(1,"productData.txt",",");
-        String[]newPrice = new String[21];
-        for(int i =0; i<price.length;i++){
+        String[] price = pd.getData(1, "productData.txt", ",");
+        String[] newPrice = new String[21];
+        for (int i = 0; i < price.length; i++) {
             newPrice = new String[i];
         }
         return price;
     }
-    
-    // getProductName() gets product name from productData.txt
+
+    /**
+     * Gets product name from productData.txt
+     *
+     * @return name
+     */
     public String[] getProductName() {
-        String[]name = pd.getData(0,"productData.txt",",");
-        String[]newName = new String[21];
-        for(int j =0; j<name.length;j++){
+        String[] name = pd.getData(0, "productData.txt", ",");
+        String[] newName = new String[21];
+        for (int j = 0; j < name.length; j++) {
             newName = new String[j];
         }
         return name;
     }
-    
-    // getProductStore() gets product store from productData.txt
+
+    /**
+     * Gets product store from productData.txt
+     *
+     * @return store
+     */
     public String[] getProductStore() {
-        String[]store = pd.getData(2,"productData.txt",",");
-        String[]newStore = new String[21];
-        for(int k =0; k<store.length;k++){
+        String[] store = pd.getData(2, "productData.txt", ",");
+        String[] newStore = new String[21];
+        for (int k = 0; k < store.length; k++) {
             newStore = new String[k];
         }
         return store;
     }
-    
-
-    
-    
 }
