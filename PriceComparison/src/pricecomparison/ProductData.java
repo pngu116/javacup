3725bbfcc340ab8c2a9b8df4e2eae6ac37b2f550
timmyttran02 <72949286+timmyttran02@users.noreply.github.com>
2023-04-11 19:31:52 -0500

    Create ProductData.java

--- PriceComparison/src/pricecomparison/ProductData.java
+++ PriceComparison/src/pricecomparison/ProductData.java
@@ -4,27 +4,39 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
 
-//reads txt file
-public class ProductData extends Database{
+/**
+ * ProductData --- program to read the ProductData.txt file.
+ *
+ * @author Khoi Tran
+ */
+public class ProductData extends Database {
+
     //gets column of data from txt file and sends to DataStorage
-    public static String[] getData(int col,String filePath,String delimiter){
+    /**
+     * Gets column of data from ProductData.txt file and sends to DataStorage
+     *
+     * @param col
+     * @param filePath
+     * @param delimiter
+     * @return colData.toArray(new String[0])
+     */
+    public static String[] getData(int col, String filePath, String delimiter) {
         String data[];
         String currentLine;
         ArrayList<String> colData = new ArrayList<String>();
-        
-        try{
+
+        try {
             FileReader fr = new FileReader(filePath);
             BufferedReader br = new BufferedReader(fr);
-            
-            while((currentLine =br.readLine())!=null){
+
+            while ((currentLine = br.readLine()) != null) {
                 data = currentLine.split(delimiter);
                 colData.add(data[col]);
             }
+        } catch (Exception e) {
+            System.out.print(e);
+            return null;
         }
-         catch(Exception e){
-             System.out.print(e);
-             return null;
-         }       
-        return colData.toArray(new String [0]);
+        return colData.toArray(new String[0]);
     }
 }
